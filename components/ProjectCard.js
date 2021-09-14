fd33af2c863c5e760e9ede0774ccf62e59351b62
JacobiClark <jacobcs@zoho.com>
2021-09-14 10:58:19 -0700

    added skills section

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -6,75 +6,73 @@ import { FaGithub, FaExternalLinkAlt } from "react-icons/fa";
 const ProjectCard = (props) => {
   console.log(props);
   return (
-    <>
-      <Flex wrap="wrap" mt="2%">
-        <Flex
-          wrap="wrap"
+    <Flex wrap="wrap" mt="2%">
+      <Flex
+        wrap="wrap"
+        border="2px"
+        borderColor="gray.200"
+        borderRadius="md"
+        p="1%"
+      >
+        <Image
           border="2px"
-          borderColor="gray.200"
+          borderColor="black"
           borderRadius="md"
-          p="1%"
-        >
-          <Image
-            border="2px"
-            borderColor="black"
-            borderRadius="md"
-            src={props.image.src}
-            alt={props.image.alt}
-            w={["100%", "100%", "50%"]}
-          ></Image>
-          <Box flex="1" p="2%">
-            <Center h="20px">
-              <Link href="/">
+          src={props.image.src}
+          alt={props.image.alt}
+          w={["100%", "100%", "50%"]}
+        ></Image>
+        <Box flex="1" p="2%">
+          <Center h="20px">
+            <Link href="/">
+              <a>
+                <Text fontSize="2xl" color="teal.500">
+                  {props.title}
+                </Text>
+              </a>
+            </Link>
+          </Center>
+          <Text fontSize="large" mt="2%">
+            {props.description}
+          </Text>
+          <Flex wrap="wrap">
+            {props.technologies.map((technology) => {
+              console.log(technology);
+              return (
+                <Flex
+                  bg="rgb(44,49,61)"
+                  borderRadius="md"
+                  align="center"
+                  p="6px"
+                  mr="8px"
+                  mt="8px"
+                  key={technology.techName}
+                >
+                  <Box mr="5px">{technology.icon}</Box>
+                  <Text mr="5px">{technology.techName}</Text>
+                </Flex>
+              );
+            })}
+          </Flex>
+          <Flex wrap="wrap" mt="4%">
+            <Box mr="10px">
+              <Link href={props.links.external}>
                 <a>
-                  <Text fontSize="2xl" color="teal.500">
-                    {props.title}
-                  </Text>
+                  <FaExternalLinkAlt size={25} />
                 </a>
               </Link>
-            </Center>
-            <Text fontSize="large" mt="2%">
-              {props.description}
-            </Text>
-            <Flex wrap="wrap">
-              {props.technologies.map((technology) => {
-                console.log(technology);
-                return (
-                  <Flex
-                    bg="rgb(44,49,61)"
-                    borderRadius="md"
-                    align="center"
-                    p="6px"
-                    mr="8px"
-                    mt="8px"
-                    key={technology.techName}
-                  >
-                    <Box mr="5px">{technology.icon}</Box>
-                    <Text mr="5px">{technology.techName}</Text>
-                  </Flex>
-                );
-              })}
-            </Flex>
-            <Flex wrap="wrap" mt="4%">
-              <Box mr="10px">
-                <Link href={props.links.external}>
-                  <a>
-                    <FaExternalLinkAlt size={25} />
-                  </a>
-                </Link>
-              </Box>{" "}
-              <Box mr="10px">
-                <Link href={props.links.gitHub}>
-                  <a>
-                    <FaGithub size={25} />
-                  </a>
-                </Link>
-              </Box>
-            </Flex>
-          </Box>
-        </Flex>
+            </Box>{" "}
+            <Box mr="10px">
+              <Link href={props.links.gitHub}>
+                <a>
+                  <FaGithub size={25} />
+                </a>
+              </Link>
+            </Box>
+          </Flex>
+        </Box>
       </Flex>
-    </>
+    </Flex>
   );
 };
 
--- components/Skills.js
+++ components/Skills.js
@@ -1,14 +1,93 @@
 import { useState } from "react";
 import { Flex, Image, Text, Box, Center } from "@chakra-ui/react";
 import Link from "next/link";
-import { SiReact, SiNextDotJs, SiTensorflow } from "react-icons/si";
-import { FaExternalLinkAlt, FaGithub } from "react-icons/fa";
+import {
+  SiGraphql,
+  SiJavascript,
+  SiMysql,
+  SiPostgresql,
+  SiPython,
+  SiJava,
+  SiReact,
+  SiNextDotJs,
+  SiHtml5,
+  SiCss3,
+  SiGit,
+  SiVisualstudiocode,
+  SiNodeDotJs,
+} from "react-icons/si";
 import ProjectCard from "./ProjectCard";
+
+const techSkills = [
+  {
+    techType: "Languages",
+    technologies: [
+      { icon: <SiJavascript />, techName: "JavaScript" },
+      { icon: <SiNodeDotJs />, techName: "Node.js" },
+      { icon: "", techName: "SQL" },
+      { icon: <SiMysql />, techName: "MySQL" },
+      { icon: <SiPostgresql />, techName: "PostgresQL" },
+      { icon: <SiGraphql />, techName: "GraphQL" },
+      { icon: <SiPython />, techName: "Python" },
+      { icon: <SiJava />, techName: "Java" },
+    ],
+  },
+  {
+    techType: "Languages",
+    technologies: [
+      { icon: <SiReact />, techName: "React" },
+      { icon: <SiNextDotJs />, techName: "Next.js" },
+      { icon: "", techName: "Chakra-UI" },
+      { icon: "", techName: "Sanity.CMS" },
+      { icon: "", techName: "GROQ" },
+      { icon: <SiReact />, techName: "React" },
+      { icon: <SiNextDotJs />, techName: "Next.js" },
+      { icon: "", techName: "Chakra-UI" },
+      { icon: "", techName: "Sanity.CMS" },
+      { icon: "", techName: "GROQ" },
+    ],
+  },
+];
 
 const Skills = () => {
   return (
-    <Box>
-      <Text>laskjdf22</Text>
+    <Box border="2px" borderColor="gray.200" borderRadius="md">
+      {techSkills.map((skillType) => {
+        return (
+          <Flex
+            key={skillType.techType}
+            wrap="wrap"
+            border="2px"
+            borderColor="black"
+            borderRadius="md"
+            m="1%"
+          >
+            <Flex align="center" m="1%">
+              <Text fontSize="xl" m="2%">
+                {skillType.techType}:
+              </Text>
+              <Flex wrap="wrap" mb="8px">
+                {skillType.technologies.map((technology) => {
+                  return (
+                    <Flex
+                      key={technology}
+                      bg="rgb(44,49,61)"
+                      borderRadius="md"
+                      align="center"
+                      p="6px"
+                      mr="8px"
+                      mt="8px"
+                    >
+                      <Box mr="5px">{technology.icon}</Box>
+                      <Text mr="5px">{technology.techName}</Text>
+                    </Flex>
+                  );
+                })}
+              </Flex>
+            </Flex>{" "}
+          </Flex>
+        );
+      })}
     </Box>
   );
 };
